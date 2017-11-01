44ee3355c3a64ba93d0d3b3d208e04f330243ff7
lizhiyuan <148334733@qq.com>
2017-11-01 10:37:13 +0800

    加上了所有的框架以及插件，项目可以开始做了

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+//home.vue引入进来
+import Home from '../views/home'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
